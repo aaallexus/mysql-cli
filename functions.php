15be624a08f8c063cb18d8f32270a32e7af82ba0
root <root@alexei-vds.vds.colocall.com>
2019-08-21 09:47:00 +0300

    add

---
+++ functions.php
@@ -0,0 +1,157 @@
+<?php
+
+function outTable($array,$limit=0){
+    $screenWidth=140;
+    $screenWidth=145;
+    $maxSizes=array();
+    if(isset($array[0]))
+    {
+        $numCols=sizeof($array[0]);
+        foreach($array[0] as $key=>$value)
+        {
+            $maxSizes[$key]=mb_strlen($key.'','utf8');
+        }
+    }
+    else
+        return '';
+    $sizeOut=0;
+    foreach($array as $key=>$value)
+    {
+        foreach($value as $i=>$vl)
+        {
+            if(is_float($value[$i]))
+            {
+                $value[$i]=sprintf('%.3f',$value[$i]);
+                $array[$key][$i]=$value[$i];
+            }
+            if($maxSizes[$i]<mb_strlen($value[$i].'','utf8'))
+                $maxSizes[$i]=mb_strlen($value[$i].'','utf8');
+        }
+    }
+    $size=0;
+    foreach($maxSizes as $value)
+    {
+        $size+=$value+4;
+    }
+    $isOversize=false;
+    if($size>$screenWidth)
+    {
+        $isOversize=true;
+        $maxSizes=array();
+        $numCols=sizeof($array[0]);
+        foreach($array[0] as $key=>$value)
+        {
+            $maxSizes[$key]=mb_strlen($key.'','utf8');
+        }
+        foreach($array as $key=>$value)
+        {
+            foreach($value as $i=>$vl)
+            {
+                if(is_float($value[$i]))
+                {
+                    $value[$i]=sprintf('%.3f',$value[$i]);
+                    $array[$key][$i]=$value[$i];
+                }
+                $temp=explode(' ',$value[$i]);
+                $localMax=0;
+                foreach($temp as $word)
+                {
+                    if($localMax<mb_strlen($word,'utf8'))
+                        $localMax=mb_strlen($word,'utf8');
+                }
+                if($maxSizes[$i]<$localMax)
+                    $maxSizes[$i]=$localMax;
+            }
+        }
+    }
+    $out="";
+    $endTable='';
+    $header='';
+    $lastSymbol='|';
+    foreach($array as $value)
+    {
+        $out.="";
+        $row='';
+        foreach($value as $i=>$vl)
+        {
+            if($isOversize)
+            {
+                #$value[$i]=str_replace(' ',"\n",$vl);
+                if(mb_strlen($value[$i],'utf8')>$maxSizes[$i])
+                {
+                    $value[$i]=mb_substr($value[$i],0,$maxSizes[$i]-1,'utf8').'..';
+                }
+            }
+           if($sizeOut==0)
+            {
+                $header.="| ".addSpaces($maxSizes[$i]+1,$i.'')."";
+            }
+            $row.="| ".addSpaces($maxSizes[$i]+1,$value[$i].'')."";
+        }
+        if($sizeOut==0)
+        {
+            if(mb_strlen($header,'utf8')>$screenWidth)
+            {
+                $lastSymbol=' ... >';
+            }
+            $out.="\n+";
+            for($j=1;($j<mb_strlen($row,'utf8') && $j<$screenWidth);$j++)
+            {
+                $out.='-';
+                $endTable.='-';
+            }
+            $out.="+\n";
+            $out.=mb_substr($header,0,$screenWidth,'utf8').$lastSymbol.$out;
+        }
+ #       for($j=1;$j<strlen($row);$j++)
+ #           $out.='-';
+        #       $out.="||\n||".$row;
+        $out.=mb_substr($row,0,$screenWidth,'utf8').$lastSymbol;
+        $out.="\n";
+        $sizeOut++;
+        if($limit>0 && $sizeOut>$limit)
+            break;
+    }
+    if($sizeOut>30)
+    {
+        $out.='+'.$endTable."+";
+        $out.="\n".mb_substr($header,0,$screenWidth,'utf8').$lastSymbol."\n";
+    }
+#    $out.="||";
+#    for($j=1;$j<strlen($row);$j++)
+#        $out.='=';
+    #     $out.="||\n";
+    $out.='+'.$endTable."+";
+    return $out;
+}
+function runQuery($query){
+
+    $keyWords=array(
+        "use",
+        "set"
+    );
+    $expl=explode(' ',$query);
+    $table=array();
+    if(in_array($expl[0],$keyWords))
+    {
+        mysql_query($query);
+    }
+    else
+    {
+        if(strlen($query)>3)
+        {
+            $quer=mysql_query($query);
+            while($res=mysql_fetch_assoc($quer))
+            {
+                $table[]=$res;
+            }
+        }
+    }
+    return $table;
+}
+function addSpaces($length,$string){
+    for($i=mb_strlen($string,'utf8');$i<$length;$i++)
+        $string=$string.' ';
+    return $string;
+}
+?>
---
+++ mysql.php
@@ -0,0 +1,68 @@
+<?php
+include 'functions.php';
+$phrases=array(
+    'use',
+    'select',
+    'show',
+    'set',
+);
+
+
+$cons='mysql';
+$dbase='';
+mysql_connect('localhost','root');
+mysql_query("set names utf8");
+$quer=mysql_query('show databases');
+while($query=mysql_fetch_array($quer))
+{
+    $phrases[]=$query[0];
+    $quer1=mysql_query('show tables in '.$query[0]);
+    while($query1=mysql_fetch_array($quer1))
+    {
+        $phrases[]=$query1[0];
+        $phrases[]=$query[0].'.'.$query1[0];
+    }
+
+}
+readline_read_history('history');
+readline_completion_function(function($input,$index){
+    global $phrases;
+    $matches=array();
+    foreach($phrases as $phrase)
+    {
+         if(stripos($phrase, $input) === 0)
+             $matches[] = $phrase;
+    }
+    if(sizeof($matches)>0)
+        return $matches;
+    if($input=='')
+        return array("");
+});
+while(true)
+{
+    $val = readline($cons.">");
+    if (!empty($val)) {
+        readline_add_history($val);
+    }
+    $table=runQuery($val);
+    if(sizeof($table)>0)
+    {
+        echo outTable($table)."\n";
+    }
+    readline_write_history('history');
+    $history=explode("\n",file_get_contents('history'));
+    $newHistory=array();
+    $keys=array();
+    for($i=sizeof($history)-1;$i>=0;$i--)
+    {
+        if(!isset($keys[$history[$i]]))
+        {
+            $keys[$history[$i]]=1;
+            $newHistory[]=$history[$i];
+        }
+    }
+    file_put_contents('history',implode("\n",array_reverse($newHistory)));
+    readline_read_history('history');
+}
+
+?>
